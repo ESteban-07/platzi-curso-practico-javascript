b148839a036c033992dd4f1e7c2cd1da87371e66
Esteban Portillo <esteban.07.elias@gmail.com>
2022-05-11 12:07:42 -0600

    mi solucion al reto #2 para descuentos con cupones

--- descuentos.js
+++ descuentos.js
@@ -1,8 +1,24 @@
-// const precioOriginal = 120;
-// const descuento = 18;
+function calcularPrecioConDescuentoDeCupon (precio, cupon) {
+    let descuento = 0;
 
+    const cupones = ["cupon-1", "cupon-2", "cupon-3"];
 
-function calcularPrecioConDescuento (precio, descuento) {
+    switch (cupon) {
+        case cupones[0]:
+            descuento += 15;
+        break;
+        case cupones[1]:
+            descuento += 25;
+        break;
+        case cupones[2]:
+            descuento += 50;
+        break;
+        default:
+            descuento = 0;
+    }
+
+    descuento *= 1;
+
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
     
@@ -10,24 +26,17 @@ function calcularPrecioConDescuento (precio, descuento) {
 }
 
 function onClickButtonPriceDiscount() {
+    const resultP = document.getElementById("ResultP");
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
+    var inputCoupon = document.getElementById("InputCoupon");
+    var couponValue = inputCoupon.value;
 
-    const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.value;
+    const priceWithDiscount = calcularPrecioConDescuentoDeCupon(priceValue, couponValue);
 
-    const priceWithDiscount = calcularPrecioConDescuento(priceValue, discountValue);
-
-    const resultP = document.getElementById("ResultP");
-    resultP.innerText += "El precio con descuento son: $" + priceWithDiscount;
+    if (priceWithDiscount == priceValue) {
+        resultP.innerHTML = "Cupón no válido :("
+    } else {
+        resultP.innerText = "El precio a pagar es de: $" + priceWithDiscount;
+    }
 }
-
-
-
-// console.log({
-//     precioOriginal,
-//     descuento,
-//     porcentajePrecioConDescuento,
-//     precioConDescuento
-// });
-
