2acdd9af2a56293d68c3ceb39b79ad0c1fa0f406
Esteban Portillo <esteban.07.elias@gmail.com>
2022-05-19 09:31:22 -0600

    cree la función calcularModa()

--- moda.js
+++ moda.js
@@ -1,33 +1,26 @@
-const lista1 = [
-    1,
-    2,
-    3,
-    1,
-    2,
-    3,
-    4,
-    2,
-    2,
-    2,
-    1
-];
+console.log(calcularModa([1, 2, 3, 1, 2, 3, 4, 2, 2, 2, 1]));
+// ['2', 5]
 
-const lista1Count = {};
+function calcularModa(arr) {
+    const listaCount = {};
 
-lista1.map(
-    function (elemento) {
-        if (lista1Count[elemento]) {
-            lista1Count[elemento] += 1;
-        } else {
-            lista1Count[elemento] = 1;
+    arr.map(
+        function (elemento) {
+            if (listaCount[elemento]) {
+                listaCount[elemento] += 1;
+            } else {
+                listaCount[elemento] = 1;
+            }
         }
-    }
-);
+    );
 
-const lista1Array = Object.entries(lista1Count).sort(
-    function (valorAcumulado = 0, nuevoValor) {
-        return valorAcumulado[1] - nuevoValor[1];
-    }
-);
+    const listaArray = Object.entries(listaCount).sort(
+        function (valorAcumulado = 0, nuevoValor) {
+            return valorAcumulado[1] - nuevoValor[1];
+        }
+    );
 
-const moda = lista1Array[lista1Array.length - 1];
+    const moda = listaArray[listaArray.length - 1];
+
+    return moda;
+}
